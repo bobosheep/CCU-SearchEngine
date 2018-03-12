7de4ac9222cc3550fcd5cbcedcbd0a2b266a461c
Ian Tsai <ianpaul32@gmail.com>
2018-03-12 17:15:00 +0800

    update

--- hw1/mainController.js
+++ hw1/mainController.js
@@ -8,8 +8,14 @@ app.controller('mainController', ['$scope','$http', '$sce', function($scope, $ht
   $scope.searchtext = {
     "content":""
   }
-  $scope.searchHost = 'http://localhost:9200/_search';
-
+  //request url
+  $scope.searchHost = {
+    "show" : false,
+    "all" :'http://localhost:9200/_search',
+    "news" : 'http://localhost:9200/test/_search',
+    "facebook" :'http://localhost:9200/facebook/_search'
+  }
+  //query body
   $scope.body = {
     "query":{
       "match" :{
@@ -27,33 +33,43 @@ app.controller('mainController', ['$scope','$http', '$sce', function($scope, $ht
   }
 
   $scope.searchPages = [{
+      "url" : '',
       "num":1,
       "display": false
     }, {
+      "url" : '',
       "num":2,
       "display": false
     }, {
+      "url" : '',
       "num":3,
       "display": false
     }, {
+      "url" : '',
       "num":4,
       "display": false
     }, {
+      "url" : '',
       "num":5,
       "display": false
     }, {
+      "url" : '',
       "num":6,
       "display": false
     }, {
+      "url" : '',
       "num":7,
       "display": false
     }, {
+      "url" : '',
       "num":8,
       "display": false
     }, {
+      "url" : '',
       "num":9,
       "display": false
     } , {
+      "url" : '',
       "num":10,
       "display": false
   }];
@@ -64,35 +80,50 @@ app.controller('mainController', ['$scope','$http', '$sce', function($scope, $ht
   }
   
   $scope.response = ''
-  $scope.search = function(val, page){
+
+  //search function
+  $scope.search = function(url, val, page){
+    if(val.length === 0){
+      console.log('Empty');
+      return;
+    }
     $scope.body.query.match.content = val;
     $scope.body.from = (page - 1) * 10;
     let total = 0;
-    $scope.response = $http.post($scope.searchHost, $scope.body,{
+
+    //post request
+    $scope.response = $http.post(url, $scope.body,{
         headers: {
           'Access-Control-Allow-Origin':'*'
         },  
       }).then(function(response){
-        //console.log(response.data);
+        ///get response
         $scope.response = response.data;
+        ///total result
         total = $scope.response.hits.total;
         $scope.searchResult.resultTotal = 'Total ' + total  + ' results' 
+        ///pages show control
         $scope.searchPages.forEach(function(p, i){
+          p.url = url
           if((p.num - 1) * 10 < total){
             p.display = true;
-            //console.log(p);
           }
           else{
             p.display = false;
           }
         });
+        ///search time show
         $scope.searchResult.searchTime = 'Search Time : ' + $scope.response.took+ ' ms'
         //$scope.highlightInfo = $scope.response.hits.hits.highlight.content.for;
+        
         return response.data.hits;
       })
       .catch(function (error) {
         console.log(error);
       });
+    
+    $scope.searchHost.show = true;
+    window.scrollTo(0, 0);
     //console.log(res);
     
   }
